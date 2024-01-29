d16226b0a1b7eafd15378a20b6a41af9fc9a4b56
GitSakshii <157839341+GitSakshii@users.noreply.github.com>
2024-01-29 11:27:17 +0530

    Update factors.java

--- factors.java
+++ factors.java
@@ -5,7 +5,7 @@ public class factors {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         boolean hasfactor=false;
-         for(int i=2;i<n;i++)
+         for(int i=2;i<=n/2;i++)
          {
              if(n%i==0)
              {
